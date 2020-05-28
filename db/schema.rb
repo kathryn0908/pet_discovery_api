a6d2bcde7b712cc8058c86f4563848dda4be4135
kathryn0908 <kathrynlael@gmail.com>
2020-05-28 14:42:28 -0600

    Updated migration table and completed Auth for login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_190732) do
+ActiveRecord::Schema.define(version: 2020_05_28_190543) do
 
   create_table "dogs", force: :cascade do |t|
     t.string "life_span"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2020_05_27_190732) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
